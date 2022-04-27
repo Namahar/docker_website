467cd120d6aa7a92a93c2a2dbefc8e6be056767a
Namahar <45704980+Namahar@users.noreply.github.com>
2022-04-27 19:43:06 -0400

    Add files via upload

---
+++ yt_dlp/extractor/extractors.py
@@ -0,0 +1,23 @@
+# flake8: noqa
+from __future__ import unicode_literals
+
+from .youtube import (
+    YoutubeIE,
+    YoutubeClipIE,
+    YoutubeFavouritesIE,
+    YoutubeHistoryIE,
+    YoutubeTabIE,
+    YoutubeLivestreamEmbedIE,
+    YoutubePlaylistIE,
+    YoutubeRecommendedIE,
+    YoutubeSearchDateIE,
+    YoutubeSearchIE,
+    YoutubeSearchURLIE,
+    YoutubeMusicSearchURLIE,
+    YoutubeSubscriptionsIE,
+    YoutubeTruncatedIDIE,
+    YoutubeTruncatedURLIE,
+    YoutubeYtBeIE,
+    YoutubeYtUserIE,
+    YoutubeWatchLaterIE,
+)
